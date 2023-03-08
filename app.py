ea9d10141f0b6391631079562b2ba39d01ebabd3
d0345853 <47771165+d0345853@users.noreply.github.com>
2023-03-08 23:59:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,77 +1,131 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
+# # -*- coding: utf-8 -*-
+# """
+# Created on Wed Jun  2 21:16:35 2021
 
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+# @author: Ivan
+# 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
-Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
-"""
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
+# Line Bot聊天機器人
+# 第一章 Line Bot申請與串接
+# Line Bot機器人串接與測試
+# """
+# #載入LineBot所需要的套件
+# from flask import Flask, request, abort
+# import openai
+# # openai.api_key = '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'
+
+
+
+# from linebot import (
+#     LineBotApi, WebhookHandler
+# )
+# from linebot.exceptions import (
+#     InvalidSignatureError
+# )
+# from linebot.models import *
+
+# app = Flask(__name__)
+
+# # 必須放上自己的Channel Access Token
+# line_bot_api = LineBotApi('ZDKxXNN1YeHrqa8+lOlgv9RjOl/2kCVpO5xoDLC3SHfnBBdA9IA3Z/fOQPiHEJhvQ9ImNXMMF/q6Dzl5Rk9UMtpi0a+NJzg+81oARe6dOeaubeXm42HCnNyGJ1j9+oBmOUj+UrZaXLYD3fYc/ybLmgdB04t89/1O/w1cDnyilFU=')
+# # 必須放上自己的Channel Secret
+# handler = WebhookHandler('91ba25530818a52375c97fbd27aac56c')
+
+# line_bot_api.push_message('Ub08558de58b09af13f8e03da6a5dfca6', TextSendMessage(text='你可以開始了'))
+
+# # 監聽所有來自 /callback 的 Post Request
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+
+#     return 'OK'
+
+# #訊息傳遞區塊
+# ##### 基本上程式編輯都在這個function #####
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)  #line input
+#     # response = openai.Completion.create(
+#     #     engine = "text-davinci-003",    # select model
+#     #     prompt = "你好",     
+#     #     max_tokens = 512,               # response tokens
+#     #     temperature = 1,                # diversity related NLG模型
+#     #     top_p = 0.75,                   # diversity related
+#     #     n = 1,                          # num of response
+#     # )
+#     # completed_text = response["choices"][0]["text"]
+#     line_bot_api.reply_message(event.reply_token,message)  #line output
+
+# completed_text = response["choices"][0]["text"]
+# print(completed_text)
+
+# #主程式
+# import os
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
+
+
 import openai
-openai.api_key = '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'
 
+from flask_ngrok import run_with_ngrok   # colab 使用，本機環境請刪除
+from flask import Flask, request
 
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+# 載入 LINE Message API 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
 
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('ZDKxXNN1YeHrqa8+lOlgv9RjOl/2kCVpO5xoDLC3SHfnBBdA9IA3Z/fOQPiHEJhvQ9ImNXMMF/q6Dzl5Rk9UMtpi0a+NJzg+81oARe6dOeaubeXm42HCnNyGJ1j9+oBmOUj+UrZaXLYD3fYc/ybLmgdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('91ba25530818a52375c97fbd27aac56c')
-
-line_bot_api.push_message('Ub08558de58b09af13f8e03da6a5dfca6', TextSendMessage(text='你可以開始了'))
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
+@app.route("/", methods=['POST'])
+def linebot():
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    json_data = json.loads(body)
+    print(json_data)
     try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        line_bot_api = LineBotApi('ZDKxXNN1YeHrqa8+lOlgv9RjOl/2kCVpO5xoDLC3SHfnBBdA9IA3Z/fOQPiHEJhvQ9ImNXMMF/q6Dzl5Rk9UMtpi0a+NJzg+81oARe6dOeaubeXm42HCnNyGJ1j9+oBmOUj+UrZaXLYD3fYc/ybLmgdB04t89/1O/w1cDnyilFU=')
+        handler = WebhookHandler('91ba25530818a52375c97fbd27aac56c')
+        line_bot_api.push_message('Ub08558de58b09af13f8e03da6a5dfca6', TextSendMessage(text='你可以開始了'))
 
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']
+        msg = json_data['events'][0]['message']['text']
+        # 取出文字的前五個字元，轉換成小寫
+        ai_msg = msg[:6].lower()
+        reply_msg = ''
+        # 取出文字的前五個字元是 hi ai:
+        if ai_msg == 'hi ai:':
+            openai.api_key = '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'
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                engine='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = msg
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(tk,text_message)
+    except:
+        print('error')
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)  #line input
-    response = openai.Completion.create(
-        engine = "text-davinci-003",    # select model
-        prompt = "你好",     
-        max_tokens = 512,               # response tokens
-        temperature = 1,                # diversity related NLG模型
-        top_p = 0.75,                   # diversity related
-        n = 1,                          # num of response
-    )
-    completed_text = response["choices"][0]["text"]
-    line_bot_api.reply_message(event.reply_token,message)  #line output
-
-completed_text = response["choices"][0]["text"]
-print(completed_text)
-
-#主程式
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    run_with_ngrok(app)   # colab 使用，本機環境請刪除
+    app.run()
