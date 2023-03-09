10714b5d65660205c388ed46efc5bd5582b537b5
d0345853 <47771165+d0345853@users.noreply.github.com>
2023-03-09 22:07:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -76,20 +76,17 @@ def handle_message(event):
     #     n = 1,                          # num of response
     # )
     #reply_msg = response["choices"][0]["text"].replace('\n','')
+
+    message_log.append({"role": "user", "content": message})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-        messages=message,   # The conversation history up to this point, as a list of dictionaries
+        messages=message_log,   # The conversation history up to this point, as a list of dictionaries
         max_tokens=2000,        # The maximum number of tokens (words or subwords) in the generated response
         stop=None,              # The stopping sequence for the generated response, if any (not used here)
         temperature=0.8,        # The "creativity" of the generated response (higher temperature = more creative)
     )
-
-    for choice in response.choices:
-        if "text" in choice:
-            reply_msg = choice.text
-            break
-    # reply_msg = response.choices[0].message.content.replace('\n','')
-
+    reply_msg = response.choices[0].message.content.replace('\n','')
+    reply_msg = response[-1]['content'].strip()
     text_message = TextSendMessage(text=reply_msg)              # 轉型
     line_bot_api.reply_message(event.reply_token,text_message)  #line output
 
