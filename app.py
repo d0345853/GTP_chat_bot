c64df829a6c4e22e19c416797a60e2e557417878
d0345853 <47771165+d0345853@users.noreply.github.com>
2023-03-12 00:06:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ handler = WebhookHandler('91ba25530818a52375c97fbd27aac56c')
 # 4.Waiting for "/callback" word from Customer Post Request (for heroku Server)
 @app.route("/callback", methods=['POST'])   
 def callback():
-    signature = request.headers['X-Line-Signature']                         # get X-Line-Signature header value
-    body = request.get_data(as_text=True)                                   # get request body as text
-    app.logger.info("Request body: " + body)
+    signature = request.headers['X-Line-Signature']                                                 # get X-Line-Signature header value
+    body = request.get_data(as_text=True)                                                           # get request body as text
+    app.logger.info("Request body: " + body)                                                        # combine
     try:
-        handler.handle(body, signature)                                     # handle webhook body
+        handler.handle(body, signature)                                                             # handle webhook body
     except InvalidSignatureError:
         abort(400)
     return 'OK'
@@ -67,9 +67,9 @@ def handle_message(event):
 
     #######################################
     # ------------ Definition ----------- #    
-    input_message = event.message.text                                      # input message (from line, String type)         
-    reply_msg = ""                                                          # output message
-    openai.api_key = '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'  # open AI account number
+    input_message = event.message.text                                                              # input message (from line, String type)         
+    reply_msg = ""                                                                                  # output message
+    openai.api_key = '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'                          # open AI account number
 
 
     # "安" "卡米" "請" "早" "兔"
@@ -92,9 +92,9 @@ def handle_message(event):
     # ------------Time and Day----------- #
     elif input_message in msgchk_timer:
         # 1. Get timer
-        time_tz = pytz.timezone('Asia/Taipei')                              # <- put your local timezone here
-        time_now = datetime.now(time_tz)                                    # the current time in your local timezone
-        time_current = time_now.strftime("%m/%d  %H:%M:%S")                 # output format
+        time_tz = pytz.timezone('Asia/Taipei')                                                      # put your local timezone here
+        time_now = datetime.now(time_tz)                                                            # the current time in your local timezone
+        time_current = time_now.strftime("%m/%d  %H:%M:%S")                                         # output format
         reply_msg = f"現在台灣時間：\n{time_current}"
         # 2. Line Output 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
@@ -102,75 +102,63 @@ def handle_message(event):
     #######################################
     # --------------Weather-------------- #
     elif ("天氣" in input_message) or ("氣象" in input_message) or ("下雨" in input_message) or ("傘" in input_message) or ("風" in input_message) :
-
         # 1.weather parameter
-        weather_code = 'CWB-86BE978B-666E-4AE1-87B6-C70A998DDD5F'           # weather API code
-        weather_list = 'F-D0047-061'                                        # Web list code (for 8 hour predict)
-        weather_output = {}                                                 # for each location
-
-        # 2.weather url link(JSON Format)
+        weather_code = 'CWB-86BE978B-666E-4AE1-87B6-C70A998DDD5F'                                   # weather API code
+        weather_output = {}                                                                         # for each location
+        # 2.getting weather url link(JSON Format)
         weather_url = f'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization={weather_code}&downloadType=WEB&format=JSON'
+        # 3.getting all weather
+        weather_data = requests.get(weather_url)                                                    # get URL
+        weather_data_json = weather_data.json()                                                     # trans json format
+        weather_location = weather_data_json['cwbopendata']['dataset']['location']                  # forecast data of major location
 
-        # if ("明天" in input_message):
-        #     weather_url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/{weather_list}?Authorization={weather_code}&elementName=WeatherDescription'
-
-        weather_data = requests.get(weather_url)   # 取得主要縣市預報資料
-        weather_data_json = weather_data.json()  # json 格式化訊息內容
-        weather_location = weather_data_json['cwbopendata']['dataset']['location']    # 取得縣市的預報內容
-        #weather_location = weather_data_json['records']['locations'][0]['location']  # 取得縣市的預報內容
+        # 4-1.parser (8 hour data) -----------------------------------------------------------------
         for i in weather_location:
-            weather_locationname = i['locationName']    # 縣市名稱
-            weather_state = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
-            #weather_min_tem = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最低溫
-            weather_max_tem = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
-            weather_comfort = i['weatherElement'][3]['time'][0]['parameter']['parameterName']    # 舒適度
-            weather_rain_prob = i['weatherElement'][4]['time'][0]['parameter']['parameterName']   # 降雨機率
+            weather_locationname = i['locationName']                                                # location (for index)
+            weather_state = i['weatherElement'][0]['time'][0]['parameter']['parameterName']         # weater status
+            weather_min_tem = i['weatherElement'][1]['time'][0]['parameter']['parameterName']       # min temperature
+            weather_max_tem = i['weatherElement'][2]['time'][0]['parameter']['parameterName']       # max temperature
+            weather_comfort = i['weatherElement'][3]['time'][0]['parameter']['parameterName']       # Comfort word
+            weather_rain_prob = i['weatherElement'][4]['time'][0]['parameter']['parameterName']     # chance of rain
+            #output 
+            weather_output[weather_locationname]=f"{weather_locationname}未來 8 小時{weather_state}，{weather_comfort}，溫度{weather_min_tem}~{weather_max_tem}度，降雨機率{weather_rain_prob}%"
 
-            weather_output[weather_locationname]=f"{weather_locationname}未來 8 小時{weather_state}，{weather_comfort}，最高溫{weather_max_tem}度，降雨機率{weather_rain_prob}%"
-
-            # if ("明天" in input_message):
-            #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{weather_locationname}未來 36 小時{weather_state}，{weather_comfort}，最高溫{weather_max_tem}度，降雨機率{weather_rain_prob}%"))
-            # else:
-            #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{weather_locationname}未來 8 小時{weather_state}，{weather_comfort}，最高溫{weather_max_tem}度，降雨機率{weather_rain_prob}%"))
-
-        # 今天資料
-        for i in weather_output:
-            if weather_name[i] in input_message:        # 如果使用者的地址包含縣市名稱
-                reply_msg = weather_output[i]  # 將 msg 換成對應的預報資訊
+        # 4-2. check input message
+        for i in weather_output:                                                                    # location list
+            if weather_name[i] in input_message:                                                    # if location name is equal to input message
+                reply_msg = weather_output[i]                                                       # output                              
                 break
             else:
-                reply_msg = weather_output["臺北市"]
-                # 將進一步的預報網址換成對應的預報網址
+                reply_msg = weather_output["臺北市"]                                                # default location
         
-        # 未來資料
+        # 5-1.parser (1 week data) -----------------------------------------------------------------
         if ("明天" in input_message) or ("後天" in input_message) or ("下星期" in input_message) or ("未來" in input_message):
-            for i in weather_output:
-                if weather_name[i] in input_message:        # 如果使用者的地址包含縣市名稱
-                    # 將進一步的預報網址換成對應的預報網址
+            for i in weather_output:                                                               # location list
+                if weather_name[i] in input_message:                                               # if location name is equal to input message
+                    # 5-2 [RE] getting weather url link(JSON Format)
                     weather_url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/{weather_list[i]}?Authorization={weather_code}&elementName=WeatherDescription'
-                    weather_data = requests.get(weather_url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
-                    weather_data_json = weather_data.json() # json 格式化訊息內容
-                    weather_location = weather_data_json['records']['locations'][0]['location']    # 取得預報內容
+                    # 5-3 [RE] getting all weather
+                    weather_data = requests.get(weather_url)                                        # get URL
+                    weather_data_json = weather_data.json()                                         # trans json format
+                    weather_location = weather_data_json['records']['locations'][0]['location']     # forecast data of major location
                     break
                 else:
-                    # 將進一步的預報網址換成對應的預報網址
+                    # default location
                     weather_url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/{weather_list["臺北市"]}?Authorization={weather_code}&elementName=WeatherDescription'
-                    weather_data = requests.get(weather_url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
-                    weather_data_json = weather_data.json() # json 格式化訊息內容
-                    weather_location = weather_data_json['records']['locations'][0]['location']    # 取得預報內容
-
+                    weather_data = requests.get(weather_url)
+                    weather_data_json = weather_data.json()
+                    weather_location = weather_data_json['records']['locations'][0]['location']
+            # 5-4 [RE] check input message
             for i in weather_location:
-                weather_locationname = i['locationName']   # 取得縣市名稱
-                weather_data = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']  # 綜合描述
-                if weather_name[i] in input_message:    # 如果使用者的地址包含鄉鎮區域名稱
-                    reply_msg = f'{weather_locationname}未來一周天氣:{weather_data}' # 將 msg 換成對應的預報資訊
-                    break
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg) )  # line output
+                weather_locationname = i['locationName']                                            # location (for index)
+                weather_data = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']        # Comprehensive data
+                if weather_name[i] in input_message:                                                # if location name is equal to input message
+                    reply_msg = f'{weather_locationname}未來一周天氣:{weather_data}'                 # output
 
     #######################################
     # --------------- Web --------------- #
     elif ("www" in input_message) or ("http" in input_message):
-        reply_msg = ""                          # no anwser
+        reply_msg = ""                                                                              # don't reply message
 
     else:
         #######################################
